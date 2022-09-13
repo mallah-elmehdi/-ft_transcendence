616c13df0aa7f396ade3cc8ccb7b8c9a2d6a1ca8
yssefnoam <2020noam@gmail.com>
2022-09-13 20:56:36 +0100

    working on Blck User UI

--- frontend/src/component/ChatHeader.tsx
+++ frontend/src/component/ChatHeader.tsx
@@ -1,29 +1,25 @@
-import React, {useContext} from 'react';
-import {Avatar, HStack, Spacer, Text} from "@chakra-ui/react";
-import {ArrowBackIcon} from "@chakra-ui/icons";
-import {ChatContext} from "../hooks/ChatProvider";
-import {ChevronDownIcon} from "@chakra-ui/icons";
-import {BsThreeDotsVertical} from  "react-icons/bs"
-import { Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider, } from '@chakra-ui/react'
-
-// import {ChatContext} from "../hooks/MessagingProvider";
+import React, { useContext } from 'react';
+import { Avatar, Box, HStack, Spacer, Text } from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { ChatContext } from '../hooks/ChatProvider';
+import { ChevronDownIcon, DeleteIcon } from '@chakra-ui/icons';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider } from '@chakra-ui/react';
+import { MdOutlineDelete } from 'react-icons/md';
 
 
 function ChatHeader() {
-    const {data} = useContext<any>(ChatContext);
-    const {selectedChat, setSelectedChat} = useContext<any>(ChatContext);
-    const {setChatDetails} = useContext<any>(ChatContext);
+    const { data } = useContext<any>(ChatContext);
+    const { selectedChat, setSelectedChat } = useContext<any>(ChatContext);
+    const { setChatDetails } = useContext<any>(ChatContext);
     let searchIndex;
 
     if (selectedChat.chat === 'F') searchIndex = data.friends.findIndex((id: any) => selectedChat.id === id.id);
     else searchIndex = data.groups.findIndex((id: any) => selectedChat.id === id.id);
     return (
-        <HStack w={'100%'} mr={5} >
-            <HStack
-                onClick={() => setChatDetails(true)}
-                as={'button'}
-                px={5} w={'100%'} m={0} h={''}>
-                <ArrowBackIcon m={0} mr={25} p={0} h={30} fontSize={25} onClick={() => setSelectedChat(null)}/>
+        <HStack w={'100%'} mr={5}>
+            <HStack onClick={() => setChatDetails(true)} as={'button'} px={5} w={'100%'} m={0} h={''}>
+                <ArrowBackIcon m={0} mr={25} p={0} h={30} fontSize={25} onClick={() => setSelectedChat(null)} />
                 <Avatar
                     // pr={25}
                     // mx={20}
@@ -32,38 +28,10 @@ function ChatHeader() {
                 ></Avatar>
                 <Text>{selectedChat.chat === 'G' ? data.groups[searchIndex].name : data.friends[searchIndex].name}</Text>
             </HStack>
-            <Spacer/>
-            <Menu>
-                {({ isOpen }) => (
-                    <>
-                        <MenuButton isActive={isOpen} as={'button'} rightIcon={<ChevronDownIcon />}>
-                            {isOpen ? 'Close' : 'Open'}
-                        </MenuButton>
-                        <MenuList>
-                            <MenuItem>Download</MenuItem>
-                            <MenuItem onClick={() => alert('Kagebunshin')}>Create a Copy</MenuItem>
-                        </MenuList>
-                    </>
-                )}
-            </Menu>
-            {/*<Menu>*/}
-            {/*    <>*/}
-            {/*    <MenuButton >*/}
-            {/*        <BsThreeDotsVertical onClick={()=>{*/}
-            {/*           console.log('Clicked three dot')*/}
-            {/*        }}  size={22}/>*/}
-            {/*    </MenuButton>*/}
-            {/*    <MenuList>*/}
-            {/*        <MenuItem onClick={()=> console.log('Download')}>Block User</MenuItem>*/}
-            {/*        <MenuItem onClick={()=> console.log('Download')}>Block User</MenuItem>*/}
-            {/*        <MenuItem onClick={()=> console.log('Download')}>Block User</MenuItem>*/}
-            {/*        <MenuItem onClick={()=> console.log('Download')}>Block User</MenuItem>*/}
-            {/*    </MenuList>*/}
-            {/*    </>*/}
-            {/*</Menu>*/}
-            {/*<BsThreeDotsVertical onClick={()=>{*/}
-            {/*   console.log('Clicked three dot')*/}
-            {/*}}  size={22}/>*/}
+            <Spacer />
+            <Box as='button'>
+                <MdOutlineDelete size={24} color="red" onClick={() => console.log('Delete User')} />
+            </Box>
         </HStack>
     );
 }
