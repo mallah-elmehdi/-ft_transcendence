74ab84d854ae4041f03b2449c0b98d8ceae6de59
yssefnoam <2020noam@gmail.com>
2022-09-14 00:01:27 +0100

    working on Block User Modal

--- frontend/src/component/ChatHeader.tsx
+++ frontend/src/component/ChatHeader.tsx
@@ -1,21 +1,37 @@
-import React, {useContext} from 'react';
-import {Avatar, Box, HStack, Spacer, Text} from '@chakra-ui/react';
-import {ArrowBackIcon} from '@chakra-ui/icons';
-import {ChatContext} from '../hooks/ChatProvider';
-import {MdOutlineDelete} from 'react-icons/md';
+import React, { useContext } from 'react';
+import { Tooltip, Avatar, Box, HStack, Spacer, Text, WrapItem, useDisclosure } from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { ChatContext } from '../hooks/ChatProvider';
+import { MdOutlineDelete } from 'react-icons/md';
+import {Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react';
 
 const BlockUser = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <Box as='button'>
-            <MdOutlineDelete size={24} color="red" onClick={() => console.log('Delete User')}/>
+        <Box as="button">
+            <MdOutlineDelete size={24} color="red" onClick={() => onOpen()} />
+            <Modal
+             onClose={onClose} size="md" isOpen={isOpen} isCentered>
+                {/* <ModalOverlay /> */}
+                <ModalContent w={'20em'} h={'10em'} >
+                    <ModalHeader>Block User</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>lkjlkjlkjlkj</ModalBody>
+                    <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
-    )
-}
+    );
+};
 
 function ChatHeader() {
-    const {data} = useContext<any>(ChatContext);
-    const {selectedChat, setSelectedChat} = useContext<any>(ChatContext);
-    const {setChatDetails} = useContext<any>(ChatContext);
+    const { data } = useContext<any>(ChatContext);
+    const { selectedChat, setSelectedChat } = useContext<any>(ChatContext);
+    const { setChatDetails } = useContext<any>(ChatContext);
     let searchIndex;
 
     if (selectedChat.chat === 'F') searchIndex = data.friends.findIndex((id: any) => selectedChat.id === id.id);
@@ -23,20 +39,15 @@ function ChatHeader() {
     return (
         <HStack w={'100%'} mr={5}>
             <HStack onClick={() => setChatDetails(true)} as={'button'} px={5} w={'100%'} m={0} h={''}>
-                <ArrowBackIcon m={0} mr={25} p={0} h={30} fontSize={25} onClick={() => setSelectedChat(null)}/>
+                <ArrowBackIcon m={0} mr={25} p={0} h={30} fontSize={25} onClick={() => setSelectedChat(null)} />
                 <Avatar
-                    // pr={25}
-                    // mx={20}
                     name={selectedChat.chat === 'G' ? data.groups[searchIndex].name.toString() : data.friends[searchIndex].name.toString()}
                     src={selectedChat.chat === 'G' ? data.groups[searchIndex].name.toString() : data.friends[searchIndex].avatar}
                 ></Avatar>
                 <Text>{selectedChat.chat === 'G' ? data.groups[searchIndex].name : data.friends[searchIndex].name}</Text>
             </HStack>
-            <Spacer/>
-            <BlockUser/>
-            {/*<Box as='button'>*/}
-            {/*    <MdOutlineDelete size={24} color="red" onClick={() => console.log('Delete User')} />*/}
-            {/*</Box>*/}
+            <Spacer />
+            <BlockUser />
         </HStack>
     );
 }
