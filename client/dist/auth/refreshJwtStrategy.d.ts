1aa4ef88e87653cc2d85f7bc13f9080919afdf26
Marouane Ougnou <mougnou@e1r9p10.1337.ma>
2022-09-29 02:19:58 +0100

    Delete Client Folder

--- client/dist/auth/refreshJwtStrategy.d.ts
+++
@@ -1,8 +0,0 @@
-import { Strategy } from 'passport-jwt';
-import { Request } from 'express';
-declare const refreshJwtStrategy_base: new (...args: any[]) => Strategy;
-export declare class refreshJwtStrategy extends refreshJwtStrategy_base {
-    constructor();
-    validate(req: Request, payload: any): any;
-}
-export {};
