67d882e1584d2dffdc23e1416d2c7bf70ed441f6
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-06 15:13:45 +0100

    cleaning

--- client/src/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './collection/Navbar'
-export { default as NavItem } from './collection/NavItem'
-export { default as SideBar } from './collection/SideBar'
-export { default as Base } from './collection/Base'
