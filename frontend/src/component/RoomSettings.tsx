09ce99b855341647e36ee7f40b2c273afcf704ea
yssefnoam <2020noam@gmail.com>
2022-10-07 23:55:51 +0100

    add: upload button to room settings

--- frontend/src/component/RoomSettings.tsx
+++ frontend/src/component/RoomSettings.tsx
@@ -1,4 +1,4 @@
-import { useColorModeValue, Box, FormControl, FormLabel, HStack, Input, VStack, Text, Spacer, Button, InputGroup, InputRightElement, IconButton } from '@chakra-ui/react';
+import { Tooltip, useColorModeValue, Box, FormControl, FormLabel, HStack, Input, VStack, Text, Spacer, Button, InputGroup, InputRightElement, IconButton } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { MdDelete } from 'react-icons/md';
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
@@ -20,13 +20,23 @@ const RoomSettings = ({ toggleSettings, roomId }: Props) => {
     };
     const oldRoomData = {
         name: 'roomName',
-        type: ROOMTYPE.private,
+        type: ROOMTYPE.protected,
         password: 'lkjlkjlkj',
-    }
+    };
     const [roomData, setRoomData] = useState<any>(oldRoomData);
     const [image, setImage] = useState(null);
 
-    const [show, setShow] = React.useState(false);
+    const [showPassword, setShowPassword] = useState<any>(false);
+
+    const [showUpload, setShowUpload] = useState<any>(false);
+    const updateRoomNewInfo = () => {
+        console.log(roomData);
+    };
+    useEffect(() => {
+        if (image != null || JSON.stringify(roomData) !== JSON.stringify( oldRoomData)) {
+            setShowUpload(true);
+        } else setShowUpload(false);
+    }, [roomData, image]);
 
     useEffect(() => {
         const keyDownHandler = (event: any) => {
@@ -42,7 +52,7 @@ const RoomSettings = ({ toggleSettings, roomId }: Props) => {
     });
 
     return (
-        <VStack w={'100%'} h={'100%'}>
+        <VStack w={'100%'} overflow={'auto'} position={'relative'} h={'100%'}>
             <HStack overflow={'visible'} px={4} w={'100%'} m={0} mb={6} spacing={8}>
                 <Box as={'button'}>
                     <ArrowBackIcon m={0} p={0} h={30} fontSize={25} onClick={toggleSettings} />
@@ -59,26 +69,38 @@ const RoomSettings = ({ toggleSettings, roomId }: Props) => {
                     <Text>Room Type</Text>
                     <RadioGroup value={roomData.type} onChange={(e: any) => setRoomData({ ...roomData, type: e })}>
                         <HStack spacing="24px">
-                            <Radio isDisabled={oldRoomData.type === ROOMTYPE.public ? true : false} value={ROOMTYPE.public}>{ROOMTYPE.public.charAt(0).toUpperCase() + ROOMTYPE.public.slice(1)}</Radio>
-                            <Radio isDisabled={oldRoomData.type === ROOMTYPE.private ? true : false}value={ROOMTYPE.private}>{ROOMTYPE.private.charAt(0).toUpperCase() + ROOMTYPE.private.slice(1)}</Radio>
-                            <Radio isDisabled={oldRoomData.type === ROOMTYPE.protected ? true : false}value={ROOMTYPE.protected}>{ROOMTYPE.protected.charAt(0).toUpperCase() + ROOMTYPE.protected.slice(1)}</Radio>
+                            <Radio isDisabled={oldRoomData.type === ROOMTYPE.public ? true : false} value={ROOMTYPE.public}>
+                                {ROOMTYPE.public.charAt(0).toUpperCase() + ROOMTYPE.public.slice(1)}
+                            </Radio>
+                            <Radio isDisabled={oldRoomData.type === ROOMTYPE.private ? true : false} value={ROOMTYPE.private}>
+                                {ROOMTYPE.private.charAt(0).toUpperCase() + ROOMTYPE.private.slice(1)}
+                            </Radio>
+                            <Radio isDisabled={oldRoomData.type === ROOMTYPE.protected ? true : false} value={ROOMTYPE.protected}>
+                                {ROOMTYPE.protected.charAt(0).toUpperCase() + ROOMTYPE.protected.slice(1)}
+                            </Radio>
                         </HStack>
                     </RadioGroup>
-                    <Button onClick={() => console.log(roomData)}>Click Me</Button>
                     {roomData.type === ROOMTYPE.protected && (
                         <FormControl>
                             <FormLabel>Password</FormLabel>
                             <InputGroup>
-                                <Input type={show ? 'text' : 'password'} pr="4.5rem" value={roomData.password} onChange={(e) => setRoomData({ ...roomData, password: e.target.value })} />
+                                <Input type={showPassword ? 'text' : 'password'} pr="4.5rem" value={roomData.password} onChange={(e) => setRoomData({ ...roomData, password: e.target.value })} />
                                 <InputRightElement width="4.5rem">
-                                    <IconButton onClick={() => setShow(!show)} aria-label="password" icon={show ? <ViewOffIcon /> : <ViewIcon />} />
+                                    <IconButton onClick={() => setShowPassword(!showPassword)} aria-label="password" icon={showPassword ? <ViewOffIcon /> : <ViewIcon />} />
                                 </InputRightElement>
                             </InputGroup>
                         </FormControl>
                     )}
                 </VStack>
-                {/* <Spacer /> */}
+                <Spacer />
                 <DeleteRoom />
+                {showUpload && (
+                    <Box position={'absolute'} right={4} bottom={4} rounded={30} onClick={updateRoomNewInfo}>
+                        <Tooltip label="add Members" openDelay={500}>
+                            <IconButton fontSize={24} w={14} h={14} rounded={30} bg={'customPurple'} variant={'ghost'} aria-label={'add Members'} color={'white'} icon={<ArrowForwardIcon />} />
+                        </Tooltip>
+                    </Box>
+                )}
             </VStack>
         </VStack>
     );
